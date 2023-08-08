66f173ae1ed9106604aaffaa79e8bdd36ca2ad34
KwonSeongMin1 <smin9011@gmail.com>
2023-08-08 16:21:36 +0900

    Option handling

--- Chapter1/argument/getopt.c
+++ Chapter1/argument/getopt.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-
-	int n,a=0,b=0,c=0;
+int main(int argc, char* argv[]){
+	
+	int n;
 	extern char *optarg;
 	extern int optind;
 
-	while((n=getopt(argc,argv,"abc:"))!=-1){
+	printf("Current Optind : %d\n", optind);
+
+	while((n=getopt(argc, argv, "abc:"))!=-1){
 		switch(n){
 			case 'a':
-				a = 1;
+				printf("Option : a\n");
 				break;
 			case 'b':
-				b = 1;
+				printf("Option : b\n");
 				break;
 			case 'c':
-				c = 1;
-				printf("optarg : %s\n",optarg);
-				break;
-			case '?':
-				printf("Unknown flag : %c", optopt);
+				printf("Option : c, Argument = %s\n", optarg);
 				break;
 		}
+		printf("Next Optind : %d\n",optind);
 	}
-
-	if(a) printf("flag : a\n");
-	if(b) printf("flag : b\n");
-	if(c) printf("flag : c, argc : %s\n",optarg);
 }
-
